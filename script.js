75c59ca482f5f4b4dc9b21e78aae41453cddd30f
Suraj Kumar <ksuraj138@gmail.com>
2024-03-25 03:15:26 +0530

    work on scroll up hide

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ let lastScrollTop = 0;
 
         window.addEventListener("scroll", function() {
             let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
             if (currentScroll > lastScrollTop) {
-                document.getElementById("top-bar").style.top = "-80px"; // Adjust height of top bar as needed
+                // Scrolling down
+                document.querySelector('.top-bar').classList.add('hidden');
             } else {
-                document.getElementById("top-bar").style.top = "0";
+                // Scrolling up
+                document.querySelector('.top-bar').classList.remove('hidden');
             }
             lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
         });
-        
